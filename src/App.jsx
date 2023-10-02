c80fc4369130e8ca906f7ac4d32d97074a40da17
Myat Inzally Khine <Inzally@Myats-MacBook-Pro-5.local>
2023-10-02 15:03:47 +0630

    final update

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import './App.css'
 import { useEffect } from 'react'
 import { useState } from 'react'
 
+
+
 function App() {
-  const[advices,setAdvices]=useState([{
-    id: '',
-    textAdvice: ''
-  }]);
+  const[advices,setAdvices]=useState({
+    id: 117,
+    textAdvice: "It is easy to sit up and take notice, what's difficult is getting up and taking action."
+  });
   
   const handleClick = async () => {
     try {
@@ -20,16 +22,14 @@ function App() {
   }
 
 
-  
-
   return (
     <>
-    <div class="h-screen bg-slate-900 flex items-center justify-center"> 
-      <div class="bg-slate-600 w-5/6 xl:w-1/3 rounded-md z-2">
-        <p class="text-green-500 text-sm text-center p-2 mt-6">ADVICE #{advices.id}</p>
-        <p class="text-white font-bold text-xl p-5 text-center">"{advices.textAdvice}"</p>
+    <div class="h-screen bg-slate-900 flex items-center justify-center "> 
+      <div class="bg-slate-600 w-5/6 xl:w-1/3 rounded-md z-2 position: relative p-3">
+        <p class="text-green-500 text-sm font-primary text-center p-2 mt-6">ADVICE # {advices.id}</p>
+        <p class="text-white text-lg font-primary p-5 text-center">"{advices.textAdvice}"</p>
         <img class="mt-2 mb-5 mx-auto" src={divider_desktop} alt="divier for advice"/>
-        <div class="flex items-center justify-center">
+        <div class="flex items-center justify-center position: absolute -bottom-6 left-0 right-0 ">
         <button onClick={handleClick} class="rounded-full bg-green-500 w-12 h-12 flex items-center justify-center"><img src={dice} alt="dice button for clicking"/></button>
         </div>
         
